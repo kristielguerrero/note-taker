14e822e5ba1aa53057c0a71a2503522c754a2622
Kris Tina <kristyguerrero20@gmail.com>
2021-07-21 14:46:27 -0500

    created the html-files.js and the apiRoutes.js files to create the paths for the htmls and the api's

---
+++ Develop/Routes/apiRoutes.js
@@ -0,0 +1,18 @@
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+const fs = require("fs");
+
+module.exports = function (app) {
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (error, data) => {
+      if (error) {
+        return error;
+      } else {
+        res.send(data);
+      }
+    });
+  });
+};
---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const htmlRoutes = require("./Routes/html-files");
+const apiRoutes = require("./Routes/apiRoutes");
+console.log(htmlRoutes);
+const express = require("express");
+const fs = require("fs");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+apiRoutes(app);
+htmlRoutes(app);
+app.listen(PORT, () => {
+  console.log("listening on port ", PORT);
+});
