3605db5a3654a7ddf925aa9ebed41ca45e91bc3b
Kris Tina <kristyguerrero20@gmail.com>
2021-07-21 15:03:19 -0500

    attempted to generate the unique id

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -2,7 +2,7 @@
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
+const generateUniqueId = require("generate-unique-id");
 const fs = require("fs");
 
 module.exports = function (app) {
